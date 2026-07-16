53c2c66523261f96156510029290663d8f564ef4
niyibiziteddy <106469231+niyibiziteddy@users.noreply.github.com>
2026-07-16 07:38:33 +0200

    Update solution - CodeHub

--- JavaScript/6-kyu/if-you-can-read-this-dot-dot-dot/if-you-can-read-this-dot-dot-dot.js
+++ JavaScript/6-kyu/if-you-can-read-this-dot-dot-dot/if-you-can-read-this-dot-dot-dot.js
@@ -1,14 +1,4 @@
 function toNato(words) {
-  console.log(words)
     let letters = words.split(' ').join('').split('')
-    let res = letters.map(item => {
-      if(![',','.','!','?'].includes(item)){
-        return NATO[item.toUpperCase()]
-      }
-      else if([',','.','!','?'].includes(item)){
-        return ""+ item
-      }
-    })
-    console.log(res.join(' '))
-    return res.join(' ');
+   return letters.map(item => [',','.','!','?'].includes(item) ?  ""+ item : NATO[item.toUpperCase()]).join(' ')
 }
