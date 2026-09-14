f4d47d20e9daa449bb912a517e569d8112a959d2
niyibiziteddy <106469231+niyibiziteddy@users.noreply.github.com>
2026-09-14 06:08:14 +0200

    Update solution - CodeHub

--- JavaScript/7-kyu/transpose-two-strings-in-an-array/transpose-two-strings-in-an-array.js
+++ JavaScript/7-kyu/transpose-two-strings-in-an-array/transpose-two-strings-in-an-array.js
@@ -1,11 +1,9 @@
 function transposeTwoStrings(array) {
     let n = array[0].length >= array[1].length ? array[0].length : array[1].length
     let str = ""
-    let x = ' '
-    let e = ''
     let b = '\n'
     for(let i =0; i<n;i++){
-        str+=`${typeof array[0][i] !== 'string' ? ' ' : array[0][i] } ${typeof array[1][i] !== 'string' ? ' ' : array[1][i]}${i === n-1? '' : b}`
+        str+=`${!array[0][i] ? ' ' : array[0][i] } ${!array[1][i] ? ' ' : array[1][i]}${i === n-1? '' : b}`
     }
   return str
 }
